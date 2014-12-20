6f5e4ff44bb5a25e6e36a1bc83e901dd6b83c145
SvenBrnn <svenbrnn@web.de>
2014-12-20 18:29:40 +0100

    increse bodyparser limit

--- config/express.js
+++ config/express.js
@@ -42,12 +42,12 @@ module.exports = function (app, passport) {
     }
 
     // bodyParser should be above methodOverride
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json({limit: '1mb'}));
+    app.use(bodyParser.urlencoded({ extended: true, limit: '1mb'  }));
     app.use(multer());
     app.use(methodOverride(function (req, res) {
         if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-        // look in urlencoded POST bodies and delete it
+            // look in urlencoded POST bodies and delete it
             var method = req.body._method;
             delete req.body._method;
             return method;
@@ -63,7 +63,7 @@ module.exports = function (app, passport) {
         secret: pkg.name,
         store: new mongoStore({
             url: config.db,
-            collection : 'sessions'
+            collection: 'sessions'
         })
     }));
     // use passport session
@@ -75,7 +75,7 @@ module.exports = function (app, passport) {
     app.use(helpers(pkg.name));
 
     //Set CORS headers
-    app.use(function(req, res, next) {
+    app.use(function (req, res, next) {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
         res.header('Access-Control-Allow-Headers', 'Content-Type');
